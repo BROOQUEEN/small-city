04f5ce78dfd26880de4fc342c63b362f128d9ff3
BROOQUEEN <dimabrooqueen@gmail.com>
2022-11-03 19:58:50 +1000

    Начал делать слайдер

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.personal__lists').slick({
+        prevArrow: '<img src="/images/icons/arrow-left.svg" alt="">',
+        nextArrow: '<img src="/images/icons/arrow-right.svg" alt="">',
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
+});
