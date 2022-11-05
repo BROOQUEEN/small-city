ef657402341afe526ea5acda87f2c9fde3b8c252
BROOQUEEN <dimabrooqueen@gmail.com>
2022-11-06 02:14:11 +1000

    Сделал по дизайну слайдер, осталось только dots

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $(function(){
-    $('.personal__lists').slick({
-        prevArrow: '<img src="/images/icons/arrow-left.svg" alt="">',
-        nextArrow: '<img src="/images/icons/arrow-right.svg" alt="">',
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1
-      });
+  $('.personal__lists').slick({
+      // dots: true,
+      prevArrow: '<img src="/images/icons/arrow-left.svg" alt="">',
+      nextArrow: '<img src="/images/icons/arrow-right.svg" alt="">',
+      infinite: true,
+      variableWidth: true,
+      slidesToShow: 3,
+      slidesToScroll: 1
+    });
 });
