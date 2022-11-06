8e0ec13d397860935b1bc81ad0495b9163cb4cd0
BROOQUEEN <dimabrooqueen@gmail.com>
2022-11-06 17:28:44 +1000

    Доделал вторйо слайдер

--- js/main.js
+++ js/main.js
@@ -9,3 +9,15 @@ $(function(){
       slidesToScroll: 1
     });
 });
+
+$(function(){
+  $('.comments__comments').slick({
+      dots: true,
+      prevArrow: '<img src="/images/icons/arrow-left.svg" alt="">',
+      nextArrow: '<img src="/images/icons/arrow-right.svg" alt="">',
+      infinite: true,
+      variableWidth: true,
+      slidesToShow: 3,
+      slidesToScroll: 1
+    });
+});
