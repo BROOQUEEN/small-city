cd02d5249a1ccc058f704cc0ebeca5ece44df9e8
BROOQUEEN <dimabrooqueen@gmail.com>
2022-11-10 17:18:21 +1000

    Сделал адаптив 4 секции (частично) на ширину экрана 320px

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(function(){
       nextArrow: '<img src="/images/icons/arrow-right.svg" alt="">',
       infinite: true,
       variableWidth: true,
-      slidesToShow: 3,
+      slidesToShow: 1,
       slidesToScroll: 1
     });
 });
@@ -21,3 +21,25 @@ $(function(){
       slidesToScroll: 1
     });
 });
+
+
+$(window).resize(function () {
+  /*ПОЛУЧАЕТ ТЕКУЩУЮ ШИРИНУ ЭКРАНА*/
+  var widthWind = $(this).width();
+
+  if (widthWind <= 320) {
+     /*КОД КОТОРЫЙ ДОЛЖЕН ВЫПОЛНЯТЬСЯ*/
+     $(function(){
+      $('.personal__lists').slick({
+          dots: true,
+          prevArrow: '<img src="/images/icons/arrow-left-320.svg" alt="">',
+          nextArrow: '<img src="/images/icons/arrow-right-320.svg" alt="">',
+          infinite: true,
+          variableWidth: true,
+          slidesToShow: 3,
+          slidesToScroll: 1
+        });
+    });
+  }
+}
+);
