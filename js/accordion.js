9023a7c13f5aee43b87cb7113b0411d871f0338f
brooqueen <dimabrooqueen@gmail.com>
2023-07-26 23:21:50 +1000

    Сделал адаптив для секции 'Только лучшие специалисты'

--- js/accordion.js
+++ js/accordion.js
@@ -3,10 +3,14 @@ document.querySelectorAll('.accordion').forEach((el) => {
 
         let content = el.nextElementSibling;
 
+        var arrow = document.querySelectorAll
+
         if (content.style.maxHeight) {
             document.querySelectorAll('.accordion-show').forEach((el) => el.style.maxHeight = null)
+            document.querySelectorAll('.personal__arrow').forEach((el) => el.style.transform = "rotate(0deg)")
         } else {
             document.querySelectorAll('.accordion-show').forEach((el) => el.style.maxHeight = null)
+            document.querySelectorAll('.personal__arrow').forEach((el) => el.style.transform = "rotate(180deg)")
             content.style.maxHeight = content.scrollHeight + 'px'
         }
     })
